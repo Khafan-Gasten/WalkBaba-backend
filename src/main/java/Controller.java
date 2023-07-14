0d792a7e5934ab8b889cf46f0ac51a7f4f544575
Max Burrows <maxajburrows@gmail.com>
2023-07-14 17:13:34 +0200

    Add controller mapping

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,13 +1,14 @@
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class Controller {
 
-    @GetMapping(path="/api")
+    @GetMapping
     ResponseEntity<String> test() {
         return ResponseEntity.ok("KG if the best fucking mob");
     }
-
 }
