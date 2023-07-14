fb3b746675ed6861a1291783c9e95eb4e5ce987d
Max Burrows <maxajburrows@gmail.com>
2023-07-14 14:41:34 +0200

    Initialise spring project

---
+++ src/main/java/com/kg/walkbababackend/WalkBabaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kg.walkbababackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkBabaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalkBabaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kg/walkbababackend/WalkBabaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kg.walkbababackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkBabaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
