67fc56e936ca9e282f55d258b376f8e44fa048a0
Max Burrows <maxajburrows@gmail.com>
2023-07-14 17:25:19 +0200

    Fix controller

--- src/main/java/DELETE/Delete.java
+++
@@ -1,4 +0,0 @@
-package DELETE;
-
-public class Delete {
-}
--- src/main/java/com/kg/walkbababackend/Controller.java
+++ src/main/java/com/kg/walkbababackend/Controller.java
@@ -1,3 +1,5 @@
+package com.kg.walkbababackend;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping
-    ResponseEntity<String> test() {
-        return ResponseEntity.ok("KG if the best fucking mob");
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("KG is the best fucking mob");
     }
 }
