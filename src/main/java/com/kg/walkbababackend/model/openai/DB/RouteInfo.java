ab367fc769e0ae23190a3faf70b0ec1fc59d9905
Max Burrows <maxajburrows@gmail.com>
2023-07-19 13:41:22 +0200

    Add some table parameters and use element collection

--- src/main/java/com/kg/walkbababackend/model/openai/DB/RouteInfo.java
+++ src/main/java/com/kg/walkbababackend/model/openai/DB/RouteInfo.java
@@ -1,7 +1,11 @@
 package com.kg.walkbababackend.model.openai.DB;
 
 
+import com.kg.walkbababackend.model.openai.DTO.WayPointDTO;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -9,6 +13,11 @@ public class RouteInfo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="corn_hund")
     private long id;
+    private String city;
+    private String country;
+    private String title;
+    private String description;
+    @ElementCollection
+    private List<String> waypoints;
 }
---
+++ src/main/java/com/kg/walkbababackend/model/openai/DTO/WayPointDTO.java
@@ -0,0 +1,6 @@
+package com.kg.walkbababackend.model.openai.DTO;
+
+import java.util.List;
+
+public record WayPointDTO(List<String> waypoints) {
+}
