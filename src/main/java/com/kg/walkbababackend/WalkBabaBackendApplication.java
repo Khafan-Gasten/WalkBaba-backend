e098a4feafff8d7d757408d77896913af0c46435
Max Burrows <maxajburrows@gmail.com>
2023-07-19 14:52:58 +0200

    Comment out test class

--- src/main/java/com/kg/walkbababackend/WalkBabaBackendApplication.java
+++ src/main/java/com/kg/walkbababackend/WalkBabaBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WalkBabaBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WalkBabaBackendApplication.class, args);
     }
