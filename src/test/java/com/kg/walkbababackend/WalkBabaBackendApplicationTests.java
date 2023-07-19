773ab5efa9c3726dc0366b692f0010264e622aaa
Max Burrows <maxajburrows@gmail.com>
2023-07-19 15:00:08 +0200

    Add class that is being tested annotation

--- src/test/java/com/kg/walkbababackend/WalkBabaBackendApplicationTests.java
+++ src/test/java/com/kg/walkbababackend/WalkBabaBackendApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.kg.walkbababackend;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class WalkBabaBackendApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.kg.walkbababackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = WalkBabaBackendApplication.class)
+class WalkBabaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
